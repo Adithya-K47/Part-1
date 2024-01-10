28f8ec8b80c19f59b766ab964f5f3c262b5d7d56
Adithya Karthik <karthiad@shernet.sheridancollege.ca>
2024-01-10 11:40:54 -0500

    Added inputs for moving and firing missle.

--- Assets/Week 1/Scripts/Mover.cs
+++ Assets/Week 1/Scripts/Mover.cs
@@ -5,16 +5,22 @@ using UnityEngine;
 public class Mover : MonoBehaviour
 {
     public float speed = 3f;
+    public GameObject misslePrefab;
+    public Transform spawn;
     // Start is called before the first frame update
     void Start()
     {
         
     }
-
+     
     // Update is called once per frame
     void Update()
     {
-        Input.GetAxis("Horizontal")
-        transform.Translate(speed * Time.deltaTime, 0, 0);
+        float direction = Input.GetAxis("Horizontal");
+        transform.Translate(direction * speed * Time.deltaTime, 0, 0);
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            Instantiate(misslePrefab, spawn.position, spawn.rotation);
+        }
     }
 }
