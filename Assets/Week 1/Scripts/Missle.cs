c9ae901d6975189191462e7ea1133f7df8c8a15b
Adithya Karthik <karthiad@shernet.sheridancollege.ca>
2024-01-17 10:31:02 -0500

    Added wall and player, missle collides with player and dissapears, (week 2)

--- Assets/Week 1/Scripts/Missle.cs
+++ Assets/Week 1/Scripts/Missle.cs
@@ -5,12 +5,12 @@ using UnityEngine;
 public class Missle : MonoBehaviour
 {
     public float speed = 3f;
-
+    Rigidbody2D rigidbody;
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        rigidbody = GetComponent<Rigidbody2D>();
     }
 
     // Update is called once per frame
@@ -18,4 +18,17 @@ public class Missle : MonoBehaviour
     {
         transform.Translate(4 * speed * Time.deltaTime, 0, 0);
     }
+
+    private void FixedUpdate()
+    {
+       Vector2 direction = new Vector2(speed * Time.deltaTime, 0);
+        rigidbody.MovePosition(rigidbody.position + direction);
+       
+    }
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+    Destroy(gameObject);
+        Debug.Log("missile collision"); 
+    }
+
 }
