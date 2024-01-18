a43d522c2c74e78d6d9f28f372d851404bd11f20
adithyak2 <karthiad@sheridancollege.ca>
2024-01-17 22:07:16 -0500

    task4b

---
+++ Assets/Week 2/Scripts/Lever.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Lever : MonoBehaviour
+{
+    public GameObject Door;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        Door.SetActive(false);
+    }
+
+    private void OnCollisionExit2D(Collision2D collision)
+    {
+        Door.SetActive(true);
+    }
+}
