9aa2d5a1cdf19f9e73170aca83fb30f36e2ff5cf
Adithya Karthik <karthiad@shernet.sheridancollege.ca>
2024-01-24 11:20:44 -0500

    Task 3 complete: car controller

---
+++ Assets/Week 3/Scripts/Car.cs
@@ -0,0 +1,35 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Car : MonoBehaviour
+{
+    Rigidbody2D rigidbody;
+    float acceleration;
+    float steering;
+    public float forwardSpeed = 300;
+    public float steeringSpeed = 30;
+    public float maxSpeed;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rigidbody = GetComponent<Rigidbody2D>();    
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        acceleration = Input.GetAxis("Vertical");
+        steering = Input.GetAxis("Horizontal");
+    }
+    private void FixedUpdate()
+    {
+        rigidbody.AddTorque(steering * -steeringSpeed * Time.deltaTime);
+        Vector2 force  = transform.up  * acceleration * Time.deltaTime;
+        if(rigidbody.velocity.magnitude< maxSpeed)
+        {
+            rigidbody.AddForce(force);
+        }
+    }
+}
