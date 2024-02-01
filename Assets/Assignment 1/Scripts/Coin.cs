f2e92b4f2675de321c29cc14196032be3d900495
Adithya-K47 <karthiad@sheridancollege.ca>
2024-01-31 20:06:59 -0500

    Made coins turn black when touched.

--- Assets/Assignment 1/Scripts/Coin.cs
+++ Assets/Assignment 1/Scripts/Coin.cs
@@ -4,15 +4,23 @@ using UnityEngine;
 
 public class Coin : MonoBehaviour
 {
+    SpriteRenderer spriteRenderer;
     // Start is called before the first frame update
     void Start()
     {
-        
+        spriteRenderer = GetComponent<SpriteRenderer>();
+        spriteRenderer.color = Color.yellow;
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+
+    }
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+
+        spriteRenderer.color = Color.black;
     }
 }
