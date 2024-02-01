4ccc6b616c361380562031242ef0e07106cdc36a
Adithya-K47 <karthiad@sheridancollege.ca>
2024-01-31 20:22:19 -0500

    Added force.

--- Assets/Assignment 1/Scripts/Skull.cs
+++ Assets/Assignment 1/Scripts/Skull.cs
@@ -5,10 +5,18 @@ using UnityEngine;
 public class Skull : MonoBehaviour
 {
     public float speed = 3f;
+    public Rigidbody2D rigidbody;
+    public float forwardForce = 6f;
+    public Transform forcePos;
     // Start is called before the first frame update
     void Start()
     {
-       
+      rigidbody = GetComponent<Rigidbody2D>();    
+    }
+
+    private void FixedUpdate()
+    {
+        
     }
 
     // Update is called once per frame
@@ -19,6 +27,13 @@ public class Skull : MonoBehaviour
 
         float hordirection = Input.GetAxis("Horizontal");
         transform.Translate(hordirection * speed * Time.deltaTime, 0, 0);
+
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            Vector3 forceDirection = transform.forward;
+            rigidbody.AddForceAtPosition(forwardForce * forceDirection, forcePos.position);
+            }
+
 
     }
 }
